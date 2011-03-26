a05c6ee517598149fdfe097f3c462c59ea5770b0
jldupont <github@jldupont.com>
2011-03-25 20:54:19 -0400

    0.8.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
     @author: Jean-Lou Dupont
 """
 __author__  ="Jean-Lou Dupont"
-__version__ ="0.8.3"
+__version__ ="0.8.4"
 
 from distutils.core import setup
 from setuptools import find_packages
